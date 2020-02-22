237b630cc3931be9ebb189fd92b1ff9d1a2cf9bc
stormymayday <stormymayday7@gmail.com>
2020-02-21 18:25:25 -0700

    changing HEADWEAR to HATS

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,7 +6,7 @@ const HomePage = () => (
         <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>HEADWEAR</h1>
+                    <h1 className='title'>HATS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
