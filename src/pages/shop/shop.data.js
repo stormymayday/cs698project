82c2ef130d45bce600ff1512994852805475e2f8
stormymayday <stormymayday7@gmail.com>
2020-03-01 16:50:07 -0700

    Added checkout page, checkout item component, and updated cart to use selectors.

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -6,8 +6,8 @@ const SHOP_DATA = [
         items: [
             {
                 id: 1,
-                name: 'Brown Brim',
-                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                name: 'Banana',
+                imageUrl: require('../../../src/assets/banana.png'),
                 price: 25
             },
             {
